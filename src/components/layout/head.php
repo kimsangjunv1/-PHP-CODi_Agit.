6312927dea6c737c74dbe7efba7a9e166ba65e41
김상준 <ingyoe64@gmail.com>
2024-10-11 03:23:26 +0900

    [Fix] SEO 보완 및 og 이미지 추가

--- src/components/layout/head.php
+++ src/components/layout/head.php
@@ -1,13 +1,15 @@
 <meta charset="UTF-8" />
 <link rel="icon" type="image/svg+xml" href="/src/assets/images/common/favicon.ico" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 <!-- 반응형 -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta content="yes" name="apple-mobile-web-app-capable" />
+<!-- 반응형 END -->
 
+<meta itemprop="name" content="코디 아지트" />
 <meta name="Date" content="2024-01-14T18:33:13+09:00" />
 <meta name="title" content="코디 아지트" />
 <meta name="robots" content="index, follow" />
@@ -16,9 +18,8 @@
 <meta name="description" content="<?php echo $seo["description"]; ?>">
 <meta name="language" content="ko" />
 <meta name="publisher" content="CODi" />
-
-<meta itemprop="name" content="코디 아지트" />
-<meta itemprop="description" content="<?php echo $seo["description"]; ?>" />
+<meta name="msapplication-TileColor" content="#ffffff" />
+<meta name="theme-color" content="#ffffff" />
 
 <!-- OG 태그 -->
 <meta property="og:url" content="https://codi-agit.com/" />
@@ -26,14 +27,18 @@
 <meta property="og:description" content="<?php echo $seo["description"]; ?>" />
 <meta property="og:site_name" content="코디 아지트" />
 <meta property="og:type" content="website" />
+<meta property="og:image" content="https://codi-agit.com/src/assets/images/common/img-og-thumbnail.png">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<!-- OG 태그 END -->
 
 <!-- 트위터 태그 -->
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:title" content="코디 아지트" />
 <meta name="twitter:description" content="<?php echo $seo["description"]; ?>" />
+<!-- 트위터 태그 END -->
 
-<meta name="msapplication-TileColor" content="#000000" />
-<meta name="theme-color" content="#000000" />
+<link rel="canonical" href="https://codi-agit.com/">
 
 <meta name="google-site-verification" content="qlarp5kejHuLMiw9Co82-ZLSGQVUy6D6ZAQUURM_UJI" />
 <meta name="naver-site-verification" content="904a6acfc526c8b6a22b2b3e3a9cc5ed4652f366" />
